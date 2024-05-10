6b87af77132386538f47273cc6e47038a86789c0
Anzherus <qqkrest0@gmail.com>
2024-05-10 05:17:26 +0300

    Solved 2 lab

---
+++ 353501/Слюсарь С.Ю./LabWork_2/Task_3/expression.h
@@ -0,0 +1,14 @@
+#ifndef EXPRESSION_H
+#define EXPRESSION_H
+#include <QDebug>
+
+
+class Expression
+{
+public:
+    Expression();
+
+    virtual double evaluate();
+};
+
+#endif // EXPRESSION_H
---
+++ 353501/Слюсарь С.Ю./LabWork_2/Task_3/headers.h
@@ -0,0 +1,7 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+#include "expression.h"
+#include "number.h"
+#include "binaryoperation.h"
+
+#endif // HEADERS_H
