35ad417087efc51cf468cb3579629654add9fbcb
Konstantin Valiulin <Clash19856@gmail.com>
2024-06-09 16:37:11 +0300

    Lab7 task1 complete

---
+++ 353504/Валиуллин К. А./LabWork_7/Task_1/node.h
@@ -0,0 +1,14 @@
+#ifndef NODE_H
+#define NODE_H
+
+template <typename T>
+class Node {
+public:
+    T data;
+    Node* next;
+    Node* prev;
+
+    Node(T value) : data(value), next(nullptr), prev(nullptr) {}
+};
+
+#endif // NODE_H
